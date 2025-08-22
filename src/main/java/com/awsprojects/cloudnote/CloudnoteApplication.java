2fb443dfe5ebd0383df786c3e6a7fdf8629e9ebc
yudhikkamboj <yudhik15@gmail.com>
2025-08-22 22:41:42 +0530

    Initial commit

---
+++ src/main/java/com/awsprojects/cloudnote/CloudnoteApplication.java
@@ -0,0 +1,13 @@
+package com.awsprojects.cloudnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class CloudnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnoteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/awsprojects/cloudnote/handler/LambdaFunctionHandler.java
@@ -0,0 +1,38 @@
+package com.awsprojects.cloudnote.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.metrics.Metrics;
+import software.amazon.lambda.powertools.tracing.CaptureMode;
+import software.amazon.lambda.powertools.tracing.Tracing;
+
+import java.util.HashMap;
+
+@Slf4j
+public class LambdaFunctionHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Logging(logEvent = true)
+    @Tracing(captureMode = CaptureMode.DISABLED)
+    @Metrics(captureColdStart = true)
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        var headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            return response
+                    .withStatusCode(200)
+                    .withBody("output");
+        } catch (Exception e) {
+            return response
+                    .withBody("{}")
+                    .withStatusCode(500);
+        }
+    }
+}
